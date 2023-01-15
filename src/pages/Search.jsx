b5bc84728e896bf7aaec0abd6eabe044e327660f
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-01-15 00:50:45 -0300

    Iniciando req 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -6,6 +6,21 @@ class Search extends Component {
     return (
       <div data-testid="page-search">
         <Header />
+        <div data-testid="page-login">
+          <input
+            type="search"
+            name="search-artist"
+            id="search-artist"
+            data-testid="search-artist-input"
+            placeholder="Digite o nome do artista"
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </div>
       </div>
     );
   }
