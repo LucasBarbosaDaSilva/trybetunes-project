2679fc9abc8863fdf559f70b2f301a14615f2622
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-01-15 22:43:26 -0300

    Req 5, criado condicional para habilitar o botão de pequisar

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    artistName: '',
+  };
+
+  saveArtistName = ({ target: { value } }) => {
+    this.setState({ artistName: value });
+  };
+
   render() {
+    const { artistName } = this.state;
+    const minimalCharacter = 2;
+
     return (
       <div data-testid="page-search">
         <Header />
@@ -13,10 +24,12 @@ class Search extends Component {
             id="search-artist"
             data-testid="search-artist-input"
             placeholder="Digite o nome do artista"
+            onChange={ this.saveArtistName }
           />
           <button
             type="button"
             data-testid="search-artist-button"
+            disabled={ artistName.length < minimalCharacter }
           >
             Pesquisar
           </button>
